5fa55075e3e1fee7a5117c019081fd10395cf130
s.siva naga parvathi <s.sivanagaparvathi@gmail.com>
2023-06-28 07:34:17 +0000

    solution

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class MatchCard extends Component {
+  render() {
+    const {matchData} = this.props
+    const {result, competingTeam, competingTeamLogo, matchStatus} = matchData
+    return (
+      <li className={`match-card ${matchStatus}`}>
+        <img
+          src={competingTeamLogo}
+          alt={`competing team ${competingTeam}`}
+          className="match-card-logo"
+        />
+        <p className="match-card-name">{competingTeam}</p>
+        <p className="match-card-result">{result}</p>
+        <p className="match-card-status">{matchStatus}</p>
+      </li>
+    )
+  }
+}
+export default MatchCard
---
+++ src/components/TeamMatches/index.js
@@ -0,0 +1,101 @@
+// Write your code here
+import './index.css'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import {Component} from 'react'
+import LatestMatch from '../LatestMatch'
+// eslint-disable-next-line no-unused-vars
+import MatchCard from '../MatchCard'
+
+class TeamMatches extends Component {
+  state = {
+    matchesData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getTeamMatches()
+  }
+
+  getTeamMatches = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
+    const fetchedData = await response.json()
+    const updatedData = {
+      teamBannerUrl: fetchedData.team_url,
+      latestMatchDetails: {
+        id: fetchedData.latestMatchDetails.id,
+        competingTeam: fetchedData.latestMatchDetails.competing_team,
+        competingTeamLogo: fetchedData.latestMatchDetails.competing_team_logo,
+        date: fetchedData.latestMatchDetails.date,
+        firstInnings: fetchedData.latestMatchDetails.first_innings,
+        manOfTheMatch: fetchedData.latestMatchDetails.man_of_the_match,
+        matchStatus: fetchedData.latestMatchDetails.match_status,
+        result: fetchedData.latestMatchDetails.result,
+        secondInnings: fetchedData.latestMatchDetails.second_innings,
+        umpires: fetchedData.latestMatchDetails.umpires,
+        venue: fetchedData.latestMatchDetails.venue,
+      },
+      recentMatches: fetchedData.recent_matches.map(recentMatch => ({
+        umpires: recentMatch.umpires,
+        result: recentMatch.result,
+        manOfTheMatch: recentMatch.man_of_the_match,
+        id: recentMatch.id,
+        date: recentMatch.date,
+        venue: recentMatch.venue,
+        competingTeam: recentMatch.competing_team,
+        competingTeamLogo: recentMatch.competing_team_logo,
+        firstInnings: LatestMatch.first_innings,
+        secondInnings: LatestMatch.second_innings,
+        matchStatus: LatestMatch.match_status,
+      })),
+    }
+    this.setState({matchesData: updatedData, isLoading: false})
+  }
+
+  renderTeamMatches = () => {
+    const {matchesData} = this.state
+    const {teamBannerUrl, latestMatchDetails} = matchesData
+    return (
+      <div className="team-matches-container">
+        <img src={teamBannerUrl} alt="team Banner" className="team-banner" />
+        <LatestMatch latestMatch={latestMatchDetails} />
+        {this.renderRecentMatchesList()}
+      </div>
+    )
+  }
+
+  renderRecentMatchesList = () => {
+    const {matchesData} = this.state
+    const {recentMatches} = matchesData
+    return (
+      <ul className="recent-matches-list">
+        {recentMatches.match(eachMatch => (
+          <MatchCard matchesData={eachMatch} key={eachMatch.id} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderLoader = () => (
+    <div testid="loader" className="loader-container">
+      <Loader type="BallTriangle" color="#00Bfff" height={80} width={80} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    return (
+      <div className={`app-team-matches-container ${id}`}>
+        {isLoading ? this.renderLoader() : this.renderTeamMatches()}
+      </div>
+    )
+  }
+}
+export default TeamMatches
